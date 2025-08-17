5b38c7100d31e9b44ab054eb1d171d7cf645f954
xpratx <71567354+xpratx@users.noreply.github.com>
2025-08-18 00:25:22 +0530

    Update apps.py

--- habit_tracker/users/apps.py
+++ habit_tracker/users/apps.py
@@ -1,5 +1,19 @@
 from django.apps import AppConfig
 import os
+from django.db.models.signals import post_migrate
+
+
+def create_superuser(sender, **kwargs):
+    if os.environ.get("CREATE_SUPERUSER") == "True":
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+
+        username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+        email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+        if username and password and not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, email=email, password=password)
 
 
 class UsersConfig(AppConfig):
@@ -7,17 +21,5 @@ class UsersConfig(AppConfig):
     name = 'habit_tracker.users'
 
     def ready(self):
-        
         import habit_tracker.users.signals
-
-        
-        if os.environ.get("CREATE_SUPERUSER") == "True":
-            from django.contrib.auth import get_user_model
-            User = get_user_model()
-
-            username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
-            email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
-            password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
-
-            if username and password and not User.objects.filter(username=username).exists():
-                User.objects.create_superuser(username=username, email=email, password=password)
+        post_migrate.connect(create_superuser, sender=self)
