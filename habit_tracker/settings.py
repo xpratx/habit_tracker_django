5285b035e10549bbe6131f68cf435515455a6430
xpratx <71567354+xpratx@users.noreply.github.com>
2025-08-17 20:52:50 +0530

    Update settings.py

--- habit_tracker/settings.py
+++ habit_tracker/settings.py
@@ -154,3 +154,12 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
+if os.environ.get("CREATE_SUPERUSER") == "True":
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    if not User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists():
+        User.objects.create_superuser(
+            username=os.environ["DJANGO_SUPERUSER_USERNAME"],
+            email=os.environ["DJANGO_SUPERUSER_EMAIL"],
+            password=os.environ["DJANGO_SUPERUSER_PASSWORD"]
+        )
