d80b288a46d823f4c3ee84d306f9f893dfd4f6f4
xpratx <71567354+xpratx@users.noreply.github.com>
2025-08-17 19:32:36 +0530

    Update urls.py

--- habit_tracker/urls.py
+++ habit_tracker/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include 
-from users import views as users_views
+from habit_tracker.users import views as users_views
 from django.conf import settings
 from django.conf.urls.static import static
 
